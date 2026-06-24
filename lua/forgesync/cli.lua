413d696b64cbb24112b3f271f4453849c2fecc41
Gabriel Vieira <nox030705@gmail.com>
2026-06-24 00:12:00 -0400

    feat(cli): sync from forgesync

--- lua/forgesync/cli.lua
+++ lua/forgesync/cli.lua
@@ -25,4 +25,29 @@ M.get_projects = function(on_done)
 	vim.system({ "forgesync", "projects", "--json" }, { text = true }, on_exit)
 end
 
+M.sync = function(on_done)
+	local on_exit = function(result)
+		local ok, decoded
+		vim.schedule(function()
+			if result.code == 0 then
+				ok, decoded = pcall(vim.json.decode, result.stdout)
+			else
+				vim.notify("Failed to sync: " .. result.stderr, vim.log.levels.ERROR)
+				return
+			end
+
+			if not ok then
+				vim.notify("Failed to decode JSON", vim.log.levels.ERROR)
+				return
+			end
+		end)
+
+		vim.schedule(function()
+			on_done(decoded.report)
+		end)
+	end
+
+	vim.system({ "forgesync", "sync", "--json" }, { text = true }, on_exit)
+end
+
 return M
