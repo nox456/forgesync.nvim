45ac83c0cb243f060e1782043a856a4d223fe066
Gabriel Vieira <nox030705@gmail.com>
2026-07-14 21:35:01 -0400

    feat(config): default configs for dashboard

--- lua/forgesync/config.lua
+++ lua/forgesync/config.lua
@@ -2,6 +2,19 @@ local M = {}
 
 M.defaults = {
 	auto_sync = true,
+	dashboard = {
+		border = "rounded",
+		max_width = 120,
+		max_height = 30,
+		keys = {
+			close = "q",
+			refresh = "r",
+		},
+		icons = {
+			pr = "",
+			synced = "",
+		},
+	},
 }
 
 M.merge = function(opts)
