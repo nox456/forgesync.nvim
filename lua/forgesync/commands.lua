e97a6684744a44657ba0ffde8a8ea80a6c234b27
Gabriel Vieira <nox030705@gmail.com>
2026-07-21 22:13:32 -0400

    feat(commands): added repo filtering in ForgeSync command

--- lua/forgesync/commands.lua
+++ lua/forgesync/commands.lua
@@ -1,10 +1,11 @@
 local sync = require("forgesync.sync")
 local dashboard = require("forgesync.dashboard")
 
-vim.api.nvim_create_user_command("ForgeSync", function()
-	sync.run()
+vim.api.nvim_create_user_command("ForgeSync", function(opts)
+	sync.run(opts.fargs[1])
 end, {
 	desc = "Perform a ForgeSync sync process",
+	nargs = 1,
 })
 
 vim.api.nvim_create_user_command("ForgeSyncDashboard", function()
